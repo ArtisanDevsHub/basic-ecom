3273901bcd125faa46f2ae88d3b099a5e12d54e0
Hm-habib <superhabib199@gmail.com>
2025-12-13 00:06:31 +0600

    added frontend and backend

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>hello</h1>
+        <h2>this secuond test </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ server/sever.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const { mongooseConnect } = require("./dbConnection/db");
+const session = require("express-session");
+const cors = require("cors");
+
+const port = 3000;
+
+mongooseConnect();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_STRING,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+
+
+require("./routes")(app);
+
+// port=3000 run to homePage
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Server has started on port ${port} `);
+});
