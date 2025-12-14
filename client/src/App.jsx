654345178e8719c848262201aca9345e78f43087
Hm-habib <superhabib199@gmail.com>
2025-12-14 22:53:54 +0600

    connection complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/api/message", {
+          withCredentials: true, // required if you use cookies/sessions
+        });
+        setMessage(res.data.text);
+      } catch (err) {
+        console.error("Error loading data:", err);
+      }
+    };
+    fetchNote();
+  }, []);
+
   return (
-    <>
-      <div>
-        <h1>hello</h1>
-        <h2>this secuond test </h2>
-      </div>
-    </>
+    <div>
+      <h1>{message}</h1>
+    </div>
   );
 }
-
-export default App;
--- server/sever.js
+++ server/sever.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const { mongooseConnect } = require("./dbConnection/db");
-const session = require("express-session");
 const cors = require("cors");
+const { mongooseConnect } = require("./dbConnection/db");
+// const session = require("express-session");
 
 const port = 3000;
 
@@ -17,35 +17,13 @@ app.use(
   })
 );
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  session({
-    secret: process.env.SESSION_STRING,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: false,
-      httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000,
-    },
-  })
-);
 
-app.use((req, res, next) => {
-  res.locals.user = req.session.user || null;
-  next();
+app.get("/api/message", (req, res) => {
+  res.json({ text: "Backend connected successfully!" });
 });
 
-app.use(express.urlencoded({ extended: true }));
-
-
-
-require("./routes")(app);
-
-// port=3000 run to homePage
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.listen(port, () => {
   console.log(`Server has started on port ${port} `);
