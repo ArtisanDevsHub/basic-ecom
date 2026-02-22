49e4a21a0080ba633a23a4d7eacef4ec618ba38a
tanjilaakter123 <Tanjilajumur24@gmail.com>
2026-02-22 22:17:33 +0600

    adminAuth updated

--- backend/src/middleware/adminAuth.js
+++ backend/src/middleware/adminAuth.js
@@ -2,30 +2,38 @@
 
 const jwt = require("jsonwebtoken")
 
-const adminAuth = async (req, res, next) => {
+ const adminAuth = async (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
+    const token = req.headers.token;
+    
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+
+    if (!token) {
       return res.status(401).json({
         success: false,
         message: "Not Authorized, login again"
       });
     }
 
-    const token = authHeader.split(" ")[1];
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-   
+    if (decoded.email !== process.env.ADMIN_EMAIL) {
+      return res.status(403).json({
+        success: false,
+        message: "Not Authorized"
+      });
+    }
+
     next();
+
   } catch (error) {
-    console.log("ADMIN AUTH ERROR ❌", error.message);
+    console.log("ADMIN AUTH ERROR ", error.message);
     res.status(401).json({
       success: false,
       message: "Invalid or expired token"
     });
   }
 };
+
 
 module.exports = adminAuth
